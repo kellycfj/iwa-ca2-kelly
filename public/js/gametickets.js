cf35df21c54376781db7d5d7784eb2d8ff73a29f
kellycfj <kellycfjesus2@gmail.com>
2022-05-27 02:10:36 +0000

    make it better

---
+++ public/js/gametickets.js
@@ -0,0 +1,60 @@
+let gameIds=[];
+let apiURL = "https://iwa-ca2k.herokuapp.com/gameticket"
+let gameSelected='';
+
+// retrieve my games
+function retrieveGames(gameId){
+    
+    let url = apiURL + gameId;
+    
+    fetch(url)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function(game){
+    
+        let strHtml = "<h1>"+game.gaelicgame+"</h1>"
+
+        strHtml+="<h2>Location: </h2>"+
+            "<h2>"+ game.moreinfos.replace(/\\n/g, "<br />"); +"</h2>"
+        $("#mygame").append(strHtml);
+
+    }).catch(function (err){
+        console.log(err);
+    }); 
+
+};
+
+//delete my game
+function delete_gameticket(){
+
+
+    if(!gameSelected == ''){
+    
+            fetch(apiURL+gameSelected,{
+                method: 'delete',
+            }).then(response => response.json()) 
+            .then(json => {
+                console.log(json)
+                newID =json._id ;
+            })
+            .catch(err => console.log(err));
+            
+            
+            document.getElementById("artists"+gameSelected).remove();
+            
+            gameIds.splice(gameIds.indexOf(gameSelected));
+            $("#mygame").empty();
+            gameSelected ='';
+            }
+    
+
+    
+};
+
+//show games  
+function showGames(gameId){
+    $("#mygame").empty();
+    showConcert(gameId);
+    gameSelected = gameId;
+};
